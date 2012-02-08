8b948650379c6afe9850f83dfb03204b35998cdf
James Waples <jamwaffles@gmail.com>
2012-02-08 12:55:56 +0000

    Can read and output latched input values

--- SerialBus.c
+++ SerialBus.c
@@ -3,16 +3,53 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+inline uint8_t readBit(volatile uint8_t port, uint8_t pin) {
+	return !((port >> pin) & 1);
+}
+
+inline void writeBit(volatile uint8_t *port, uint8_t pin, uint8_t value) {
+	if(value) {
+		*port &= ~(1 << pin);
+	} else {
+		*port |= (1 << pin);
+	}
+}
+
 void init() {
 	DDRB = 0xff;
 	PORTB = 0x00;
+
+	DDRD = 0x00;
+	DDRC = 0x00;
+
+	PORTD = 0x00;
+	PORTC = 0x00;
+}
+
+uint8_t input() {
+	uint8_t result = 0;
+
+	writeBit(&result, 0, readBit(PINC, PC2));
+	writeBit(&result, 1, readBit(PIND, PD0));
+	writeBit(&result, 2, readBit(PIND, PD1));
+	writeBit(&result, 3, readBit(PIND, PD2));
+
+	return result;
+}
+
+// This works, stop fucking about with it
+void output(uint8_t value) {
+	uint8_t mask = 0b11110000;
+
+	value <<= 4;
+
+	PORTB = (PORTB & ~mask) | (value & mask);
 }
 
 int main(void) {
 	init();
 
 	while(1) {
-		PORTB++;
-		_delay_ms(10);
+		output(input());
 	}
 }
